02e0943416a62f48b6e2dd34ef5b91ef6cf1cda0
kark1982 <karkohb@gmail.com>
2025-05-02 11:20:24 +0000

    Update Bright.py

--- Bright.py
+++ Bright.py
@@ -3,7 +3,7 @@ import pandas as pd
 from googletrans import Translator
 
 # Load the dataset from GitHub
-url = "https://github.com/kark1982/AGRICULTURAL-CHATBOT-FOR-BRIGHT-AND-FIRDAUS/blob/main/Book1.csv"
+url = "https://github.com/kark1982/chatbot-for-farmers/blob/main/Book1.csv"
 df = pd.read_csv(url)
 
 # Ensure column names are correctly formatted
